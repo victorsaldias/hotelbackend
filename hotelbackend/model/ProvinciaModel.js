9b1075b9fb5e73387c06b39418afd6e57caf00d0
victorsaldias <viiktor.gb@gmail.com>
2025-12-10 05:32:15 -0300

    camvio en backend

--- hotelbackend/model/ProvinciaModel.js
+++ hotelbackend/model/ProvinciaModel.js
@@ -1,28 +1,17 @@
+// model/ProvinciaModel.js
 import { getConnection } from "../config/dbConfig.js";
 
-export const Provincia = {
-    idProvincia: 0,
-    nombre: "",
-    idRegion: 0
-};
+export async function obtenerProvinciasPorRegion(idRegion) {
+    const pool = await getConnection();
 
+    const result = await pool.request()
+        .input("idRegion", idRegion)
+        .query(`
+            SELECT idProvincia, nombre, idRegion
+            FROM Provincia
+            WHERE idRegion = @idRegion
+            ORDER BY nombre
+        `);
 
-export const obtenerProvinciasPorRegion = async (req, res) => {
-    try {
-        const { idRegion } = req.params;
-
-        const pool = await getConnection();
-        const result = await pool.request()
-            .input("idRegion", idRegion)
-            .query(`
-                SELECT idProvincia, nombre, idRegion
-                FROM provincia
-                WHERE idRegion = @idRegion
-            `);
-
-        res.json(result.recordset);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Error obteniendo provincias" });
-    }
-};
+    return result.recordset; // SIEMPRE devuelve ARRAY
+}
